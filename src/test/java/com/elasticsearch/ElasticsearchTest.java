8744367c03faf9686d6d690679660c6f4a650a6e
Sean Maxwell <semaxwell@corelogic.com>
2021-08-28 15:22:44 -0500

    Added aggregations,pagination,sorting,data loader

---
+++ src/test/java/com/elasticsearch/ElasticsearchTest.java
@@ -0,0 +1,12 @@
+package com.elasticsearch;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ElasticsearchTest {
+
+    /*
+     * TODO - Implement queries as tests
+     */
+
+}
